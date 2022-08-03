d64d6d530d3ef132bcb5811c757eeddf01635d61
Felix Folayan <bossfelfo@gmail.com>
2022-08-03 16:02:46 +0100

    code review and style implementation

--- src/layouts/NavBar/MobileNavBar.tsx
+++ src/layouts/NavBar/MobileNavBar.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { CloseIcon, HamburgerIcon } from '../../components/icons';
+import { closeMobileNav } from '../../features/app/appSlice';
 import { useAppDispatch } from '../../store';
 // import Header from '../Header/Header';
 import NavBar from './NavBar';
 import styles from './NavBar.module.scss';
+import Logo from '../../assets/MyNews.png';
+import SearchBar from '../SearchBar/SearchBar';
 
 const MobileNavBar = ({}) => {
   const dispatch = useAppDispatch();
   return (
     <div className={styles.mobileNav}>
-      <NavBar />
+      {/* <div
+        onClick={(e) => {
+          e.preventDefault();
+          dispatch(closeMobileNav());
+        }}
+        className={styles.close}
+      >
+        <CloseIcon />
+      </div> */}
+      <div className={styles.logo}>
+        <Link to="/">
+          <img src={Logo} alt="MyNews" />
+        </Link>
+      </div>
+      <div className={styles.search}>
+        <SearchBar />
+      </div>
+      <div className={styles.nav}>
+        <NavBar />
+      </div>
     </div>
   );
 };
